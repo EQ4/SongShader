5fad159d05be2b2430a0afcfb97551c51e9cd90e
Brenton Milne <brenton.a.milne@gmail.com>
2015-09-18 19:45:16 +1200

    SDL Init

--- SongShader.py
+++ SongShader.py
@@ -20,15 +20,29 @@ class SsGl:
 
 class SsSdl:
     def __init__(self):
-        SDL_Init(SDL_INIT_EVERYTHING)
-        SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1)
-        self.screen = SDL_SetVideoMode(800, 600, 32, SDL_OPENGL)
-        SDL_WM_SetCaption("SongShader")
+        SDL_Init(SDL_INIT_VIDEO)
+
+        # Use OpenGL 3.1 core
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3)
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1)
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE)
+
+        self.window = SDL_CreateWindow(
+            b"SongShader", 
+            SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 
+            SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN
+            )
+        self.context = SDL_GL_CreateContext(self.window)
+
+        # Use Vsync
+        SDL_GL_SetSwapInterval(1)
+
+
 
 class SsApp:
     def __init__(self):
-        self.gl = SsGl()
         self.sdl = SsSdl()
+        self.gl = SsGl()
 
 
 if __name__ == '__main__':
